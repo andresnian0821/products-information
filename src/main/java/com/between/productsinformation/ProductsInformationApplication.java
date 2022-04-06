8572abbc63e227298d95ec943a700271666c2572
andresnian0821 <eduardo.nino@rappi.com>
2022-04-06 16:18:06 -0500

    First commit project

---
+++ src/main/java/com/between/productsinformation/ProductsInformationApplication.java
@@ -0,0 +1,13 @@
+package com.between.productsinformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsInformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsInformationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/between/productsinformation/ProductsInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.between.productsinformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsInformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
