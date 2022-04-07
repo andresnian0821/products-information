6b8a4af928220c7734647911b15e182af80f8099
andresnian0821 <eduardo.nino@rappi.com>
2022-04-07 12:33:32 -0500

    sub project app configuration

--- products-information-app/src/test/java/com/between/products/information/app/ProductsInformationApplicationTests.java
+++ products-information-app/src/test/java/com/between/products/information/app/ProductsInformationApplicationTests.java
@@ -1,4 +1,4 @@
-package com.between.productsinformation;
+package com.between.products.information.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
